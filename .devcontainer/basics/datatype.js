841b666b509a733db14a86fac7a72fa643e976b8
bhavishy09 <futuresingh111@gmail.com>
2025-02-26 00:34:07 +0530

    add data type

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "bhavishya"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  example if you need no temperature so 0 is temp but you use null
+// undefined => 
+// symbol => unique
+
+
+// object
+ console.table([name,age,isLoggedIn,state])
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
