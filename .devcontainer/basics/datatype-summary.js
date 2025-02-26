561cedf83d8a0e1ad91e5b1fcc0ce94bc5e0b8e5
bhavishy09 <futuresingh111@gmail.com>
2025-02-26 12:16:05 +0530

    add more

--- .devcontainer/basics/datatype-summary.js
+++ .devcontainer/basics/datatype-summary.js
@@ -50,3 +50,40 @@ console.log(typeof null);
 
 //refer for interview 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//memory stack and heap 
+
+//stack (primitive )=> y copy store krta h 
+
+let anotherUser ="bhavishya katariya "
+
+let userAnother=anotherUser
+console.log(userAnother)
+
+userAnother="future singh"
+console.log(anotherUser)
+console.log(userAnother)
+
+
+// see there is copy go // by cahngeing of another user no change in user another //copy value milta h 
+
+//heap(fot non -primitive )=> by refernce value milta h 
+
+let userone={
+    id:101,
+    name:"king"
+
+}
+
+let usertwo =userone
+
+console.log(usertwo.name)
+
+//w
+
+usertwo.name="raja"
+//see both change user one or two becuse yha value refence se change ho rhi h 
+console.log(usertwo.name)
+console.log(userone.name)
+
