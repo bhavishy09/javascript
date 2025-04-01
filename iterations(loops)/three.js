5462f7b3a0059ef9966ce6cd80463895bbf23a55
bhavishy09 <futuresingh111@gmail.com>
2025-04-02 02:05:38 +0530

    high order loop

---
+++ iterations(loops)/three.js
@@ -0,0 +1,27 @@
+// for of loop--> don't need size and and i++ and all it automatacally fetch 
+
+//[{},{},{},{}]
+
+
+//how this look -->
+// for (const iterator of object) {
+    // }
+
+    //it applicable on arr string objects  etc.
+
+//example
+   
+const arr=[1,2,3,4,5,6]
+    for (const num of arr) {
+
+        console.log(num)
+        
+    }
+
+    //output 1 2 3 4 5 6 give --> this how it works 
+
+    const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
