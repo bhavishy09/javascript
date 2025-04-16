10c3e413bce3c3f8136f54d0f42d4043c2b49a20
bhavishy09 <futuresingh111@gmail.com>
2025-04-16 14:22:57 +0530

    call keyword

---
+++ .devcontainer/oops/call.js
@@ -0,0 +1,21 @@
+
+// CALL--> K BARE M 
+
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+
+    // SYNTAX CALL KEYWORD USE KRNA PADEGA 
+    SetUsername.call(this, username)
+   
+
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
